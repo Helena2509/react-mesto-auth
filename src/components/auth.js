950b5fbfda4733fafdaf6347829657c3bedca696
Kasitskaya Elena <keal.gyurie7@yandex.ru>
2020-10-25 01:01:49 +0300

    gfhjhj

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://api.kea.students.nomoreparties.space';
+export const BASE_URL = 'https://www.api.kea.students.nomoreparties.space';
 
 export const register = (password, email) => {
   return fetch(`${BASE_URL}/signup`, {
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,7 @@
 import Api from './Api.js'
 
 const api = new Api({
-  baseUrl: 'http://api.kea.students.nomoreparties.space'
+  baseUrl: 'https://www.api.kea.students.nomoreparties.space'
 });
 
 export default api;
